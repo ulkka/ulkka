0e91104146a8166a586b430d7d367f5c0b211852
Arjun <arjun@Arjuns-MacBook-Pro.local>
2021-04-09 13:48:21 +0530

    firebase upgrade and rnfirebase/perf & crashlytics setup

--- src/client/mainClient.js
+++ src/client/mainClient.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {axiosConfig} from './axiosConfig';
 import createAuthRefreshInterceptor from 'axios-auth-refresh';
 import auth from '@react-native-firebase/auth';
+import perf from '@react-native-firebase/perf';
 
 // Function that will be called to refresh authorization
 // new token will be added to header in idTokenListener.js
@@ -35,6 +36,59 @@ function getUrl(config) {
   }
   return config.url;
 }
+
+// RNFirebase Perf interceptors start
+mainClient.interceptors.request.use(async function (config) {
+  try {
+    const httpMetric = perf().newHttpMetric(config.url, config.method);
+    config.metadata = {httpMetric};
+
+    // add any extra metric attributes, if required
+    // httpMetric.putAttribute('userId', '12345678');
+
+    await httpMetric.start();
+  } finally {
+    return config;
+  }
+});
+
+mainClient.interceptors.response.use(
+  async function (response) {
+    try {
+      // Request was successful, e.g. HTTP code 200
+
+      const {httpMetric} = response.config.metadata;
+
+      // add any extra metric attributes if needed
+      // httpMetric.putAttribute('userId', '12345678');
+
+      httpMetric.setHttpResponseCode(response.status);
+      httpMetric.setResponseContentType(response.headers['content-type']);
+      await httpMetric.stop();
+    } finally {
+      return response;
+    }
+  },
+  async function (error) {
+    try {
+      // Request failed, e.g. HTTP code 500
+
+      const {httpMetric} = error.config.metadata;
+
+      // add any extra metric attributes if needed
+      // httpMetric.putAttribute('userId', '12345678');
+
+      httpMetric.setHttpResponseCode(error.response.status);
+      httpMetric.setResponseContentType(error.response.headers['content-type']);
+      await httpMetric.stop();
+    } finally {
+      // Ensure failed requests throw after interception
+      return Promise.reject(error);
+    }
+  },
+);
+
+// RNFirebase Perf interceptors end
 
 // Intercept all requests
 /*mainClient.interceptors.request.use(
