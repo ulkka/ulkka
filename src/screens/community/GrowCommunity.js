736df567c11e1bffb18590e72d507ccf4104f207
Arjun <arjun@Arjuns-MacBook-Pro.local>
2021-06-29 19:40:26 +0530

    grow community screen pdate

--- src/screens/community/GrowCommunity.js
+++ src/screens/community/GrowCommunity.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text, Platform, TouchableOpacity} from 'react-native';
+import {View, Text, Platform, ImageBackground} from 'react-native';
+import {Icon} from 'react-native-elements';
 import ShareCommunity from './ShareCommunity';
 import AutolinkText from '../../components/AutolinkText';
 import {useSelector} from 'react-redux';
@@ -16,78 +17,96 @@ export default function GrowCommunity(props) {
       style={{
         flex: 1,
         backgroundColor: '#fff',
-        padding: 20,
-        justifyContent: 'center',
+        justifyContent: 'space-evenly',
       }}>
-      <Text
+      <ImageBackground
+        blurRadius={0.5}
+        resizeMode="repeat"
         style={{
-          fontSize: 15,
-          fontWeight: 'bold',
-          color: '#424242',
-
-          ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
-        }}>
-        1) Invite users to join your community from their profile page in Ulkka
-      </Text>
-      <View style={{height: 30}}></View>
-      <Text
-        style={{
-          fontSize: 15,
-          fontWeight: 'bold',
-          color: '#424242',
-          ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
-        }}>
-        2) Let others know about the community by sharing the community's invite
-        link on various social media platforms
-      </Text>
-      <View
-        style={{
-          paddingVertical: 5,
-          paddingHorizontal: 10,
-          borderRadius: 10,
-          alignSelf: 'flex-start',
-          marginTop: 10,
-          backgroundColor: '#289df4',
-        }}>
-        <ShareCommunity
-          communityId={communityId}
-          text={'Share Invite Link'}
-          flexDirection={'row'}
-          mode="light"
-          shareTextStyle={{
-            fontWeight: 'bold',
-            fontSize: 15,
-            paddingLeft: 5,
-          }}
-        />
-      </View>
-      <View style={{height: 30}}></View>
-      <AutolinkText
-        text={
-          '3) Mention #' +
-          communityTitle +
-          '  in comments of relevant posts to help more people discover the community'
-        }
-        source="growCommunity"
-        enableShowMore={false}
-        textStyle={{
-          fontSize: 15,
-          fontWeight: 'bold',
-          color: '#424242',
-          ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
+          //  flex: 1,
+          width: '100%',
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
         }}
-      />
-      <View style={{height: 30}}></View>
-      <Text
-        style={{
-          fontSize: 15,
-          fontWeight: 'bold',
-          color: '#424242',
-          ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
-        }}>
-        4) You can also increase the community ranking to show up in the top
-        community recommendation by creating content and engagement regularly
-      </Text>
+        source={require('../../../assets/doodlebg.jpg')}>
+        <View>
+          <Icon name="rocket" type="font-awesome" size={120} color="red" />
+        </View>
+        <View style={{padding: 20}}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 'bold',
+              color: '#424242',
+
+              ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
+            }}>
+            1) Invite users to join your community from their profile page in
+            Ulkka
+          </Text>
+          <View style={{height: 30}}></View>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 'bold',
+              color: '#424242',
+              ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
+            }}>
+            2) Let others know about the community by sharing the community's
+            invite link on various social media platforms
+          </Text>
+          <View
+            style={{
+              paddingVertical: 5,
+              paddingHorizontal: 10,
+              borderRadius: 10,
+              alignSelf: 'flex-start',
+              marginTop: 10,
+              backgroundColor: '#289df4',
+            }}>
+            <ShareCommunity
+              communityId={communityId}
+              text={'Share Invite Link'}
+              flexDirection={'row'}
+              mode="light"
+              shareTextStyle={{
+                fontWeight: 'bold',
+                fontSize: 16,
+                paddingLeft: 5,
+              }}
+            />
+          </View>
+          <View style={{height: 30}}></View>
+          <AutolinkText
+            text={
+              '3) Mention ' +
+              communityTitle +
+              ' in comments of relevant posts to help more people discover the community'
+            }
+            source="growCommunity"
+            enableShowMore={false}
+            textStyle={{
+              fontSize: 16,
+              fontWeight: 'bold',
+              color: '#424242',
+              ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
+            }}
+          />
+          <View style={{height: 30}}></View>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 'bold',
+              color: '#424242',
+              ...(Platform.OS == 'android' && {fontFamily: 'roboto'}),
+            }}>
+            4) You can also increase the community ranking to show up in the top
+            community recommendation by creating content and engagement
+            regularly
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
